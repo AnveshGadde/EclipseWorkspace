d53ad1f16d534071bfd026b1ff6ec9e588341eea
Anvesh Gadde <anveshkumar.gadde@gmail.com>
2016-08-22 13:53:01 -0400

    Changes made

--- ThreadRunnable/src/org/anveshgadde/mutlithreading/threadrunnable/ThreadRunnable.java
+++ ThreadRunnable/src/org/anveshgadde/mutlithreading/threadrunnable/ThreadRunnable.java
@@ -1,15 +1,23 @@
 package org.anveshgadde.mutlithreading.threadrunnable;
 
-public class ThreadRunnable implements Runnable {
+public class ThreadRunnable extends Thread implements Runnable {
 
 	@Override
 	public void run() {
-		System.out.println("Runnable thread Started!");
+		System.out.println("Thread has been instantiated!");
 	}
 
-	public static void main(String[] args) {
-		ThreadRunnable trbl = new ThreadRunnable();
-		Thread trt = new Thread(trbl);
-		trt.run();
+	public static void main(String[] args) throws InterruptedException {
+
+		ThreadRunnable tr = new ThreadRunnable();
+		System.out.println("Thread running from extend Thread method");
+		tr.start();
+		Thread.sleep(1000);
+
+		Thread trd = new Thread(tr);
+		System.out.println("Thread running from implemented Runnable method");
+		trd.start();
+
 	}
+
 }
